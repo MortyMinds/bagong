56bb8735b152a7c34cce51376f7918c897548228
LongLongLiveTheKing <132038482+MortyMinds@users.noreply.github.com>
2023-04-28 20:59:43 -0700

    Updates

---
+++ precache-manifest.ca5ea2067f633bea4064e821fe4a4068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e630584e03e429ed7a40",
+    "url": "/bagong/static/css/main.c9fc68fb.chunk.css"
+  },
+  {
+    "revision": "e630584e03e429ed7a40",
+    "url": "/bagong/static/js/main.e630584e.chunk.js"
+  },
+  {
+    "revision": "f2796b6f91badbf2dc11",
+    "url": "/bagong/static/js/1.f2796b6f.chunk.js"
+  },
+  {
+    "revision": "ae3bf99bc07433954750",
+    "url": "/bagong/static/js/runtime~main.ae3bf99b.js"
+  },
+  {
+    "revision": "b6499ea1bb01c1e155b5bb55141666a8",
+    "url": "/bagong/static/media/pickle_rick.b6499ea1.png"
+  },
+  {
+    "revision": "6a9d4df1e913cfd50376b8e5fdc94174",
+    "url": "/bagong/index.html"
+  }
+];
